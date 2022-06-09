8560aae45844cce04bfedc0993fd50428f3d739f
mohainakonda <mohasinakonda39@gmail.com>
2022-06-09 22:32:51 +0600

    netlify not found page added

--- src/Componnts/Projects/Projects.js
+++ src/Componnts/Projects/Projects.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal p-0">
+          <li>
+            <Link to="/mern-project">MERN</Link>
+          </li>
+
+          <li>
+            <Link to="react-project">React project</Link>
+          </li>
+          <li>
+            <Link to="javascript">Javascript project</Link>
+          </li>
+        </ul>
+      </div>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Projects;
